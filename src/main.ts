5d2541ae0e1e30b59c63ce701ba102edb33b1a52
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-02-15 11:07:36 +0100

    feat: mise en place de swagger dans le main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('HapplyPets')
+    .setDescription(
+      'Laissez votre animal avoir un HAPPLY day avec HAPPLY PETS !',
+    )
+    .setVersion('1.0')
+    .addTag('Animals')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
